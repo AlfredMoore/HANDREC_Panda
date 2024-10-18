35f73c64625396b3702bc6f60e82246989155782
Mo Xu <xmo@cs.wisc.edu>
2024-10-18 14:59:37 -0500

    update submodule franka_panda_description

--- init.sh
+++ init.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/bash
 
+# change the owner of cargo
 sudo chown -R $(whoami):$(whoami) $CARGO_HOME
+
+# install dependencies
+sudo rosdep update
+sudo rosdep install --from-paths src --ignore-src -r -y
